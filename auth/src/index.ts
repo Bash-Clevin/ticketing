f15b0cfc0bb621c47a04427308b5ed476e39b55f
Bash-Clevin <clevinbash@gmail.com>
2023-08-29 10:40:38 +0300

    feat: Handle async errors using express-errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
+import 'express-async-errors';
 import { currentUserRouter } from './routes/currentUser';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/errorHandler';
+import { NotFoundError } from './errors/notFoundError';
 
 const app = express();
 app.use(json());
@@ -13,6 +15,9 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*', () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {
