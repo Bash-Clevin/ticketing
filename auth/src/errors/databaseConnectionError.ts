2f1f36aa55f8925fc1ffa7ffef6ba81677a5a64b
Bash-Clevin <clevinbash@gmail.com>
2023-08-29 10:23:35 +0300

    feat: Handle errors using classes

---
+++ auth/src/errors/databaseConnectionError.ts
@@ -0,0 +1,19 @@
+import { CustomError } from './customError';
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connecting to database';
+  statusCode = 500;
+  constructor() {
+    super('Error logging into database');
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/requestValidationError';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Hello');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be > 4 chars'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    res.send({});
+  },
+);
 
 export { router as signupRouter };
