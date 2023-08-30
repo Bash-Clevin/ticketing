9ba2938fff56124146ebe2513ec1428a51afe675
kevin otieno <kevin.otieno@megatherium.solutions>
2023-08-30 07:52:13 +0300

    feat: Add signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,56 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validateRequest';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/badRequestError';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hello');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Provide valid password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    // check if user exists
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // compare passwords
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password,
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      'abcdefgh', //modify to use env
+    );
+
+    // Store jwt on session
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  },
+);
 
 export { router as signinRouter };
